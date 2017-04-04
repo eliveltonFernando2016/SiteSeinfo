b7f73e7bc20635fbab6e0110b611a7660cd1312b
elivelton <elivelton@elivelton>
2017-04-04 11:09:54 -0300

    Revertendo Merge do Romulo.

--- routes/lotes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('lotes', { title: 'SEINFO17' });
-});
-
-module.exports = router;
